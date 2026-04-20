e92dbc91f5b5f1c248fff63fdfefe2107635b6dd
Lotanna <tannaloamobi@gmail.com>
2026-04-20 08:37:25 +0100

    Add borrowBook and returnBook methods to Book class for book state updating

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -3,4 +3,54 @@ public class Book {
     private String title;
     private String author;
     boolean isBorrowed;
+
+    // constructor - 2 args
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isBorrowed() {
+        return isBorrowed;
+    }
+
+    public void setBorrowed(boolean borrowed) {
+        isBorrowed = borrowed;
+    }
+
+
+    public String borrowBook(){
+
+        if (isBorrowed()){
+            return "This book has been borrowed already.";
+        } else {
+            setBorrowed(true);
+            return "This book is available, you can borrow this book";
+        }
+    }
+
+    public String returnBook(){
+        if (!isBorrowed()){
+            return "This book has not been borrowed yet, cannot return!";
+        } else {
+            setBorrowed(false);
+            return "This book has been returned";
+        }
+    }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        Book hoodFeminism = new Book("Hood Feminism", "Mikki Kendall");
+        System.out.println(hoodFeminism.borrowBook());
+        System.out.println(hoodFeminism.isBorrowed);
+        System.out.println(hoodFeminism.returnBook());
+        System.out.println(hoodFeminism.returnBook());
+    }
 }
